c853b85a43e42e18fa80d66753e21e99da5d5de5
Silvia <silviatmnez@gmail.com>
2021-12-11 14:18:20 +0000

    map component added

---
+++ src/front/js/component/map.js
@@ -0,0 +1,56 @@
+import React, { useRef, useCallback } from "react";
+import GoogleMapReact from "google-map-react";
+
+import PropTypes from "prop-types";
+
+const Marker = () => (
+	<div>
+		<i className="fas fa-map-marker-alt" style={{ fontSize: "30px", color: "red" }} />
+	</div>
+);
+
+const Label = props => (
+	<div className="border border-dark rounded p-2 bg-white" style={props.styleLabel}>
+		<p style={{ fontWeight: "bold" }}>{props.name}</p>
+		<p>{props.direction}</p>
+	</div>
+);
+
+const Map = props => {
+	const style = {
+		width: "fit-content",
+		height: "fit-content",
+		position: "absolute",
+		left: -props.width / 2 + 10,
+		top: -props.height / 2 + 10,
+		textAlign: "left"
+	};
+	console.log(style.left);
+	return (
+		<div style={{ width: props.width + "px", height: props.height + "px" }}>
+			<GoogleMapReact
+				bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+				defaultCenter={{ lat: props.lat, lng: props.lng }}
+				defaultZoom={10}
+				yesIWantToUseGoogleMapApiInternals>
+				<Label name={props.name} direction={props.direction} styleLabel={style} />
+				<Marker lat={props.lat} lng={props.lng} />
+			</GoogleMapReact>
+		</div>
+	);
+};
+Map.propTypes = {
+	lat: PropTypes.number,
+	lng: PropTypes.number,
+	width: PropTypes.string,
+	height: PropTypes.string,
+	name: PropTypes.string,
+	direction: PropTypes.string
+};
+Label.propTypes = {
+	name: PropTypes.string,
+	direction: PropTypes.string,
+	styleLabel: PropTypes.object
+};
+
+export default Map;
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
-import icon from "../../img/icon.png";
 import "../../styles/home.css";
+import Map from "../component/map.js";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
@@ -36,10 +36,14 @@ export const Home = () => {
 			</div>
 			<br />
 			<div className="row">
-				<div className="offset-3 col-6 offset-3">
-					<iframe
-						src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26432.43434605815!2d-118.34398771265504!3d34.09374955803937!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2bf07045279bf%3A0xf67a9a6797bdfae4!2sHollywood%2C%20Los%20%C3%81ngeles%2C%20California%2C%20EE.%20UU.!5e0!3m2!1ses!2ses!4v1638286747086!5m2!1ses!2ses"
-						style={{ width: "600px", height: "350px" }}
+				<div className="d-flex justify-content-center">
+					<Map
+						lat={26.01128}
+						lng={-80.142967}
+						width="600"
+						height="350"
+						name="Hollywood"
+						direction="Hollywood Blvd Hollywood, CA 90028"
 					/>
 				</div>
 			</div>
